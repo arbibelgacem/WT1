df6b6e11c2aaf4f3dbff76cc16648ab0661b113d
arbi_maatoug <arbibelgacem111@gmail.com>
2023-02-16 13:01:53 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,12 @@
 package ma.enset;
+/*
+ * 
+ * comment
+ */
+
 public class Test{
     public static void main(String[] args) {
+        //new Comment...................
         System.out.println("Hello Git !");
     }
 }
